5f880d3e6be5d600ac4d8987b01eb5a9bdfcab44
emreglc <emre.gulec.2002@gmail.com>
2024-02-08 01:57:56 +0300

    cart length is displayed, alerts are added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,13 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import ShoppingCart from './ShoppingCart'
+import { useSelector } from 'react-redux'
 
 export default function Navbar() {
 
     const [cartActive, setCartActive] = useState(false)
+
+    const cart = useSelector((state) => state.cart);
 
     return (
         <div className="navbar bg-lime-300 flex justify-between px-12 py-8 align-middle sticky h-32">
@@ -15,7 +18,13 @@ export default function Navbar() {
                 <Link to={"/"}><FontAwesomeIcon icon={faCss3Alt} size='3x' /></Link>
             </div>
             <div className="right flex">
-                <button className="p-3" onClick={() => { setCartActive(!cartActive) }}><FontAwesomeIcon icon={faCartShopping} size='2x' /></button>
+                <button className="p-3 relative" onClick={() => { setCartActive(!cartActive) }}><FontAwesomeIcon icon={faCartShopping} size='2x' />
+                    {cart.length > 0 &&
+                        <span className="rounded-full border-blue-950 border-2 h-6 w-6 inline-block text-sm bg-slate-300 absolute bottom-1 right-0">
+                            {cart.length}
+                        </span>
+                    }
+                </button>
                 <ShoppingCart cartActive={cartActive} />
             </div>
         </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,7 @@
 import { useDispatch } from 'react-redux'
 import { addToCart } from '../redux/stores/slices/cart'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
 export default function Product({ name, price, id }) {
 
@@ -17,7 +19,14 @@ export default function Product({ name, price, id }) {
                 <p className="text-gray-700 text-base">${price}</p>
             </div>
             <div className="px-6 py-4">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(addToCart({ name, price, id }))}>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {
+                    dispatch(addToCart({ name, price, id }));
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success!',
+                        text: 'Product added to cart successfully',
+                    });
+                }}>
                     Add to Cart
                 </button>
             </div>
