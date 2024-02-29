57636f6916df910948a06618727349b92e47b6d0
emreglc <emre.gulec.2002@gmail.com>
2024-02-29 22:17:58 +0300

    logout button is added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,13 +4,16 @@ import { faArrowRightToBracket, faCartShopping } from '@fortawesome/free-solid-s
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import ShoppingCart from './ShoppingCart'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../redux/stores/slices/auth'
 
 export default function Navbar() {
 
     const [cartActive, setCartActive] = useState(false)
 
     const cart = useSelector((state) => state.cart);
+    const { user } = useSelector((state) => state.auth);
+    const dispatch = useDispatch()
 
     return (
         <div className="navbar bg-lime-300 flex justify-between px-12 py-8 align-middle sticky h-32">
@@ -18,11 +21,19 @@ export default function Navbar() {
                 <Link to={"/"}><FontAwesomeIcon icon={faCss3Alt} size='3x' /></Link>
             </div>
             <div className="right flex">
-                <span className="p-3 relative">
-                    <Link className="flex flex-col text-lg font-medium" to="/auth" >
-                        <FontAwesomeIcon icon={faArrowRightToBracket} size='2x' /><h4>LOGIN</h4>
-                    </Link>
-                </span>
+                {user ?
+                    <span className="p-3 relative">
+                        <button className="flex flex-col text-lg font-medium" onClick={() => dispatch(logout())} >
+                            <FontAwesomeIcon icon={faArrowRightToBracket} size='2x' /><h4>LOGOUT</h4>
+                        </button>
+                    </span>
+                    :
+                    <span className="p-3 relative">
+                        <Link className="flex flex-col text-lg font-medium" to="/auth" >
+                            <FontAwesomeIcon icon={faArrowRightToBracket} size='2x' /><h4>LOGIN</h4>
+                        </Link>
+                    </span>
+                }
                 <button className="p-3 relative" onClick={() => { setCartActive(!cartActive) }}><FontAwesomeIcon icon={faCartShopping} size='2x' />
                     {cart.length > 0 &&
                         <span className="rounded-full border-blue-950 border-2 h-6 w-6 inline-block text-sm bg-slate-300 absolute bottom-1 right-0">
